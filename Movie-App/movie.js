38b20e49173b6a6a0cfd8db5b0060103996d751c
Brennen Schwab <brennen@admins-MacBook-Pro.local>
2021-07-16 16:42:42 -0700

    jQuery

---
+++ Movie-App/movie.js
@@ -0,0 +1,42 @@
+let currMovie = 0;
+let movieList = [];
+
+$(function() {
+
+  $("form").on("submit", function(evt) {
+
+    evt.preventDefault();
+    let title = $('#title').val();
+    let rating = $('#rating').val();
+
+    let movieData = { title, rating, currMovie };
+    const HTMLtoAppend = createMovieDataHTML(movieData);
+
+    currMovie++
+    movieList.push(movieData);      
+
+    $("#table-body").append(HTMLtoAppend);
+    $("#movie-form").trigger("reset");
+  });
+
+  $("tbody").on("click", function(evt) {
+   
+    $(evt.target)
+      .closest("tr")
+      .remove();
+    
+  });
+});
+function createMovieDataHTML(data) {
+  return `
+    <tr>
+      <td>${data.title}</td>
+      <td>${data.rating}</td>
+      <td>
+        <button class="btn" data-delete-id=${data.currMovie}>
+          Delete
+        </button>
+      </td>
+    <tr>
+  `;
+}
